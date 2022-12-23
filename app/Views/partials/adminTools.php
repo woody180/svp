fc829fb2364cedf911e1da1a98224d941af69bf9
woody180 <woody180@rocketmail.com>
2022-12-23 13:42:25 +0400

    admin tools partial

---
+++ app/Views/partials/adminTools.php
@@ -0,0 +1,3 @@
+<div class="uk-position-fixed uk-position-bottom-right uk-margin-medium-bottom uk-margin-medium-right uk-position-z-index">
+    <a href="<?= baseUrl("back/articles") ?>" uk-icon="icon: file-text; ratio: .9" class="uk-flex uk-flex-middle uk-flex-center uk-border-circle uk-background-default uk-box-shadow-medium" data-style="width: 50px; height: 50px;"></a>
+</div>
