6ad4a712b147d25f7412a0d5cdcfbcd73da3e625
woody180 <woody180@rocketmail.com>
2022-12-23 13:42:53 +0400

    add middleware to the articles route

--- app/Routes/ArticlesRoute.php
+++ app/Routes/ArticlesRoute.php
@@ -5,12 +5,11 @@ use App\Engine\Libraries\Router;
 
 $router = Router::getInstance();
 
-$router->get('article/new',                'ArticlesController@new');
-$router->post('article',                   'ArticlesController@create');
+$router->get('article/new',                'ArticlesController@new', 'Middlewares/checkAdmin');
+$router->post('article',                   'ArticlesController@create', 'Middlewares/checkAdmin');
 $router->get('article',                    'ArticlesController@index');
 $router->get('article/(:segment)',         'ArticlesController@show');
-$router->get('article/(:segment)/edit',    'ArticlesController@edit');
-$router->put('article/(:segment)',         'ArticlesController@update');
-$router->patch('article/(:segment)',       'ArticlesController@update');
-$router->delete('article/(:segment)',      'ArticlesController@delete');
-        
+$router->get('article/(:segment)/edit',    'ArticlesController@edit', 'Middlewares/checkAdmin');
+$router->put('article/(:segment)',         'ArticlesController@update', 'Middlewares/checkAdmin');
+$router->patch('article/(:segment)',       'ArticlesController@update', 'Middlewares/checkAdmin');
+$router->delete('article/(:segment)',      'ArticlesController@delete', 'Middlewares/checkAdmin');
