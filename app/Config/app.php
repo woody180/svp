c157ae191fd33d0d6c41e8c5a530c9b788b5708e
woody180 <woody180@rocketmail.com>
2022-12-23 13:56:02 +0400

    csrf protection

--- app/Config/app.php
+++ app/Config/app.php
@@ -4,6 +4,6 @@ define("APPEMAIL", NULL);
 define("ERROR_HANDLING", TRUE);
 date_default_timezone_set("Asia/Tbilisi"); // https://www.php.net/manual/en/timezones.php
 define("FORCE_SECURE_REQUESTS", FALSE);
-define("CSRF_PROTECTION", FALSE);
+define("CSRF_PROTECTION", TRUE);
 define("CSRF_REFRESH", FALSE);
 define("MULTILINGUAL", FALSE);
