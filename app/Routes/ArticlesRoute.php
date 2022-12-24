794a2fb2eacb073ba0adc829fd8d6015cd6d53fa
woody180 <woody180@rocketmail.com>
2022-12-24 13:53:39 +0400

    add middlewares

--- app/Routes/ArticlesRoute.php
+++ app/Routes/ArticlesRoute.php
@@ -7,7 +7,7 @@ $router = Router::getInstance();
 
 $router->get('article/new',                'ArticlesController@new', 'Middlewares/checkAdmin');
 $router->post('article',                   'ArticlesController@create', 'Middlewares/checkAdmin');
-$router->get('article',                    'ArticlesController@index');
+$router->get('article',                    'ArticlesController@index', 'Middlewares/checkAdmin');
 $router->get('article/(:segment)',         'ArticlesController@show');
 $router->get('article/(:segment)/edit',    'ArticlesController@edit', 'Middlewares/checkAdmin');
 $router->put('article/(:segment)',         'ArticlesController@update', 'Middlewares/checkAdmin');
